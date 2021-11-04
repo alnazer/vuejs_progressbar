487e449501b7061dbf7c265c8b2a2595c0bdd7aa
alnazer <alnazer@users.noreply.github.com>
2021-11-04 23:12:16 +0300

    finsting add color

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,14 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" width="25%" />
     <ProggressBar :progress="100" />
-    <HelloWorld msg="Hello Vue in CodeSandbox!" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
 import ProggressBar from "./components/ProgressBar";
 export default {
   name: "App",
   components: {
-    HelloWorld,
     ProggressBar,
   },
 };
